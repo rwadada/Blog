21778246886ab8a7498e10575bcdf741ae0c7bab
rwadada <wadada.knct@gmail.com>
2026-02-23 23:30:38 +0900

    fix

--- feature/common/src/commonMain/kotlin/TitleLine.kt
+++ feature/common/src/commonMain/kotlin/TitleLine.kt
@@ -1,15 +1,28 @@
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TitleLine() {
-    Row(Modifier.fillMaxWidth()) {
+    val animationProgress = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        animationProgress.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 600)
+        )
+    }
+
+    Row(Modifier.fillMaxWidth(animationProgress.value)) {
         Box(
             modifier = Modifier
                 .weight(1f)
